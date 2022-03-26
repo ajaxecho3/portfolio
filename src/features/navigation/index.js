993fbba4556ac122e207afa0ba59557ae6659fb9
bernad <ajaxecho3@gmail.com>
2022-03-27 02:57:43 +0800

    fix\

--- src/features/navigation/index.js
+++ src/features/navigation/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Container, Nav, Button } from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 export default function Navigation() {
 
   return (
