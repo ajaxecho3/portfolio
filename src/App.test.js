3adc43e7f71dc09128ae44fcbd8c3c0a3ff1ed21
bernad <ajaxecho3@gmail.com>
2022-04-04 05:12:28 +0800

    fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Bernardino/i);
+  const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
 });
